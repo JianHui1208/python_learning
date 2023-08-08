b4bf30dad108aad9a780b4d3fc749a70e7207adf
JianHui1208 <jianhuiwilliam80@gmail.com>
2023-08-08 19:41:50 +0800

    Update Chatbot

--- py_chatbot/app.py
+++ py_chatbot/app.py
@@ -1,50 +1,82 @@
-#imports
 import os
+import json
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+from chatterbot.trainers import ListTrainer
+
 app = Flask(__name__, template_folder='template', static_folder='static')
 
-os.remove("db.sqlite3")
+class ChatbotConfig:
+    NAME = "Camilla"
+    STORAGE_ADAPTER = "chatterbot.storage.SQLStorageAdapter"
+    DATABASE_URI = "sqlite:///my_database.db"  # SQLite database URI
+    LOGIC_ADAPTERS = [
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ]
 
-#创建chatbot 叫做catBot
-catBot = ChatBot("Camilla", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+def create_chatbot(config):
+    chatbot = ChatBot(
+        config.NAME,
+        storage_adapter=config.STORAGE_ADAPTER,
+        database_uri=config.DATABASE_URI,  # Set the database URI
+        logic_adapter=config.LOGIC_ADAPTERS
+    )
+    return chatbot
 
-bots = [catBot]
-for c in bots :
-    # Chatterbot自带的语言库
-    trainer = ChatterBotCorpusTrainer(c)
+def train_chatbot(chatbot, training_data):
+    trainer = ListTrainer(chatbot)
+    trainer.train(training_data)
 
-    # 训练语言库
-    trainer.train("chatterbot.corpus.english")
+# Load configuration
+chatbot_config = ChatbotConfig()
 
-# 使用自己设定的语言库
-cat_data = open("training/cat.txt").read().splitlines()
-catTrainer = ListTrainer(catBot)
-catTrainer.train(cat_data)
+# Create and train the chatbot
+chatbot = create_chatbot(chatbot_config)
+training_data = open("training/cat.txt").read().splitlines()
+train_chatbot(chatbot, training_data)
 
-# Catbot是训练的聊天机器人
-bot_dict = {"cat":catBot}
-bot_name = "cat"
+# Create and train the chatbot
+chatbot = create_chatbot(chatbot_config)
+training_data = open("training/cat.txt").read().splitlines()
+train_chatbot(chatbot, training_data)
 
-#define app routes
+# Define app routes
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/cat")
-def cat():
-    bot_name = "cat"
     return render_template("bunny.html")
 
 @app.route("/get")
-
-#function for the bot response
 def get_bot_response():
     userText = request.args.get('msg')
-    bot = bot_dict.get(bot_name)
-    return str(bot.get_response(userText))        
+    if userText == "byebye":
+        shutdown_server()
+        return str("byebye")
+    else:
+        return process_user_input(userText)
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+def process_user_input(userText):
+    bot_response = chatbot.get_response(userText)
+    
+    response_data = {
+        "text": bot_response.text,
+        "confidence": bot_response.confidence,
+        "in_response_to": [str(s) for s in bot_response.in_response_to]
+    }
+    
+    result = {"status_code": 990, "message": "Processed successfully", "data": response_data}
+    
+    return_as_json = True
+    if return_as_json:
+        return json.dumps(result)
+    else:
+        return result
 
 if __name__ == "__main__":
-    # app.debug = True
     app.run(port=7000)
